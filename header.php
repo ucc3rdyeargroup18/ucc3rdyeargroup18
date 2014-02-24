3350353dc86cb00ef777d955263cb03e8f0eb2e3
cathaltoomey <111302591@umail.ucc.ie>
2014-02-24 16:30:38 +0000

    Create header.php

---
+++ header.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Header template for charity systems manager
+ * Outputs beginning of HTML document
+ * @author Cathal Denis Toomey (111302591)
+ */
+
+require_once 'database_functions.php';
+connect_to_database();
+
+require_once 'getDetails.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?=$headers['Header1']?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/sticky-footer-navbar.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Wrap all page content here -->
+    <div id="wrap">
+        
+        <?php
+        require_once 'navBar.php';
+        ?>
