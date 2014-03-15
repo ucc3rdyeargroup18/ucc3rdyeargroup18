712f51739f40acab70ccd89f0301eb48c8a2f21a
seanjeremiahwallace <seanjeremiahwallace@users.noreply.github.com>
2014-03-15 13:02:19 +0000

    Update home.php

--- code/charity/home.php
+++ code/charity/home.php
@@ -19,21 +19,8 @@ require_once 'header.php';
             <h1><?=$headers['Header1']?></h1>
           </div>
         <div class="col-md-9">
-            
-            <p class="lead"><?=$content['text1']?></p>
             <?php
-            //remove 
-            $numItems = count($headers);
-            if(count($content) > $numItems){
-                $numItems = count($content);
-            }
-            for($i=2; $i < $numItems; $i++){//start from 2 to skip content that has already been output
-                if(isset($headers['Header' . $i]))//if the header exists and != null
-                    echo '<h2>' . $headers['Header' . $i] . '</h2>';
-                if(isset($content['text' . $i]))//if the content exists and != null
-                    echo '<p>' . $content['text' . $i] . '</p>';
-            }
-            
+                  echo '<p>' . $charityDetails['description'] . '<p>';
             ?>
         </div>
           <?php
