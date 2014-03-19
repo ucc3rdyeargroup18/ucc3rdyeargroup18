4341c29c9191c362ff4d8dd957233486ba1d2bb7
cathaltoomey <111302591@umail.ucc.ie>
2014-03-19 01:33:14 +0000

    Update navBar.php

--- code/navBar.php
+++ code/navBar.php
@@ -1,14 +1,17 @@
 <?php
-
 /* 
  * @Author: Cathal Denis Toomey (1113025911) <111302591@umail.ucc.ie>
  * Script to create dynamic navigation bar 
  */
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start(); //start a session if one does not exist
+}
+require 'getDetails.php';
 ?>
 
 <!-- Fixed navbar -->
-<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+<div class="navbar navbar-default navbar-fixed-top" id="navBarCol" role="navigation">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -17,14 +20,15 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"><?=$info['Name']?></a>
+      <a class="navbar-brand" href="/<?=$charityDomain?>/home"><?=$info['Name']?></a>
     </div>
     <div class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
           <?php
             $currentURLRaw = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL);
             $currentURLArray = explode('/', $currentURLRaw);
-            $currentURL = $currentURLArray[2];
+            $arrayLength = count($currentURLArray);
+            $currentURL = $currentURLArray[$arrayLength-1];
             //loop through the array of pages to be added to the nav bar
             /*foreach($info['nav'] as $title => $url){
                 if(!is_array($url)){
@@ -47,27 +51,49 @@
             }*/
             
             foreach($info['nav'] as $page){
+                $explode = explode('.', $page['FileName']);
+                $explodeLen = count($explode);
                 echo "<li";
                 echo $page['FileName'] == $currentURL ? ' class="active"' : ''; //if this is the current page, apply active class
-                echo "><a href=\"{$page['FileName']}\">";
+                if($explode[$explodeLen-1] == "php"){
+                    echo  "><a href=\"{$page['FileName']}\">";
+                } else {
+                    echo "><a href=\"/{$charityDomain}/{$page['FileName']}\">";
+                }
                 echo $page['CustomTitle'] == '' ? $page['Name'] : $page['CustomTitle'];
                 echo"</a>";
             }
           ?>
       </ul>
         
-        <div class="col-sm-3 col-md-4 pull-right" style="padding-top: 8px; padding-bottom: 8px;">
-            <form class="navbar-search pull-right" action="login.php" method="post">
-                <div class="input-group">
-                    <input type="email" class="form-control login-form-control" style="width:49%;" placeholder="Email Address" name="email">
-                    <input type="password" class="form-control login-form-control" style="width:49%;" placeholder="Password" name="password">
-                    <div class="input-group-btn login-btn">
-                        <input type="submit" value="Login" class="btn"/>
+            <?php
+              if($currentURL != "login.php"):
+                if(isset($_SESSION['validUser']) && $_SESSION['validUser']):
+                    echo '<div class="col-sm-3 col-md-4 pull-right">';
+                    echo '<ul class="nav navbar-nav pull-right">';
+                    echo '<li>';
+                    echo '<a href="/' . $charityDomain . '/cms">Go to Admin Panel</a>';
+                    echo '</li>';
+                    echo '<li>';
+                    echo '<a href="/logout.php">Logout</a>';
+                    echo '</li></ul>';
+                else:
+           ?>
+            <div class="col-sm-3 col-md-4 pull-right" style="padding-top: 8px; padding-bottom: 8px;">
+                <form class="navbar-search pull-right" action="/login.php" method="post">
+                    <div class="input-group">
+                        <input type="email" class="form-control login-form-control" style="width:49%;" placeholder="Email Address" name="email">
+                        <input type="password" class="form-control login-form-control" style="width:49%;" placeholder="Password" name="password">
+                        <input type="hidden" name="redirectURL" value="<?=$currentURLRaw?>">
+                        <div class="input-group-btn login-btn">
+                            <input type="submit" name="submit" value="Login" class="btn"/>
+                        </div>
                     </div>
-                </div>
-                
-            </form>
-          </div>
+
+                </form>
+            <?php endif;
+            echo '</div>';
+                endif; ?>
     </div><!--/.nav-collapse -->
   </div>
 </div>
