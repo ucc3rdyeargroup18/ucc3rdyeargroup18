2f69e81988af245bbb00d79f22481604c628b9f6
cathaltoomey <111302591@umail.ucc.ie>
2014-03-20 11:37:48 +0000

    Update listEvent.php

--- code/charity/listEvent.php
+++ code/charity/listEvent.php
@@ -38,7 +38,7 @@ if(isset($_GET['anml'])){
           <?php 
           $count = 0;
 
-        $getEventsSQL = "SELECT Name, Description, DATE_FORMAT(Start, '%e-%b-%y %H:i') AS Start, DATE_FORMAT(End, '%e-%b-%y %H:i') AS End, Location, Image1, ID FROM CMS_Events WHERE CharityID = {$info['CharityID']} AND End > NOW() ORDER BY Start";
+         $getEventsSQL = "SELECT Name, Description, DATE_FORMAT(Start1, '%e-%b-%y %H:%i') AS Beginning, DATE_FORMAT(End1, '%e-%b-%y %H:%i') AS Finish, Location, Image1, EventID FROM CMS_Events WHERE CharityID = {$info['CharityID']} AND End1 > NOW() ORDER BY Beginning";
         $result = mysql_query($getEventsSQL);
         while($row = mysql_fetch_assoc($result))
         {
@@ -51,21 +51,21 @@ if(isset($_GET['anml'])){
                 echo '<div class="row lost-found-row">';
             }
             echo '<div class="col-lg-4">
-              <img class="img-circle" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;" src="/';
+              <img class="img-circle" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;" src="/images/events/';
             //TODO change image src to BLOB ($animal['Image1'])
             //echo $animal['Image1']; //display the image of the missing animal
             //echo data_uri($animal['Image1'], 'image/jpeg');
-            echo 'images/clucker.jpg';
+            echo $event['Image1'];
             echo '">
               <h2>';
             echo $event['Name']; //print the animals name
             echo'</h2>';
               echo $event['Description'];
               echo '</br>';
-              echo 'Location: ' + $event['Location'];
+              echo 'Location: ' . $event['Location'];
               echo '</br>';
-              echo $event['start'] + '-' + $event['end'];
-              echo '<p><a class="btn btn-default" href="eventDetails?eventID=' . $event['ID'];
+              echo $event['Beginning'] . ' - ' . $event['Finish'];
+              echo '<p><a class="btn btn-default" href="eventDetails?eventID=' . $event['EventID'];
               //echo $animal['animalPermaID'];
               echo '" role="button">View details »</a></p>
             </div><!-- /.col-lg-4 -->';
