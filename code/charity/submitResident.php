df893accc4156050df7e332bc9b2c9eba02fdc72
cathaltoomey <111302591@umail.ucc.ie>
2014-03-21 01:30:39 +0000

    Update submitResident.php

--- code/charity/submitResident.php
+++ code/charity/submitResident.php
@@ -21,9 +21,7 @@ if(isset($_POST['submission']) && $_POST['submission'] === "true"){ // the form 
     $number = filter_input(INPUT_POST, 'number', FILTER_SANITIZE_STRING);
     $petDetails['Email'] = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
     $petDetails['Location'] = trim(filter_input(INPUT_POST, 'location', FILTER_SANITIZE_STRING));
-    
-    //TODO handle file uploads
-    
+        
     if($petDetails['Name'] === ""){
         $errors['name'] = "Please enter the animal's name";
     }
@@ -45,12 +43,89 @@ if(isset($_POST['submission']) && $_POST['submission'] === "true"){ // the form 
         $petDetails['Number'] = $number;
     }
     
+    //process the story
+    $newStory = filter_input(INPUT_POST, 'newStory', FILTER_VALIDATE_BOOLEAN);
+    $existingStory = filter_input(INPUT_POST, 'existingStory', FILTER_VALIDATE_BOOLEAN);
+    
+    if($newStory){
+        $storyName = filter_input(INPUT_POST, 'storyName', FILTER_SANITIZE_STRING);
+    } elseif($existingStory){
+        $storiesSQL = "SELECT * FROM cms_stories WHERE CharityID = (SELECT CharityID FROM cms_charities WHERE DomainName = '{$_SESSION['lastDomain']}');";
+        $storiesResult = mysql_query($storiesSQL);
+        $options = array();
+        while($row = mysql_fetch_assoc($storiesResult)){
+            $options[] = $row['StoryID'];
+        }
+        $storyID = filter_input(INPUT_POST, 'existingStoryID', FILTER_VALIDATE_INT, $options);
+        if(!$storyID){
+            $errors['story'] = "Please choose a valid story";
+        }
+    } //else not using story
+    
+    $allowedTypes = array(
+        "image/gif",
+        "image/jpeg",
+        "image/jpg",
+        "image/pjpeg",
+        "image/x-png",
+        "image/png"
+    );
+    if($_FILES['img1']['size'] <= 0){
+        $errors['img1'] = "Image 1 is required";
+    } elseif($_FILES['img1']['error'] > 0){
+        //there is an error
+        $errCode = $_FILES['img1']['error'];
+        if($errCode == 1 || $errCode == 2){
+            $errors['img1'] = "The file uploaded for Image 1 is too large.";
+        } elseif($errCode == 3){
+            $errors['img1'] = "There was an error uploading Image 1";
+        } elseif ($errCode == 4){
+            $errors['img1'] = "No file was supplied for Image 1";
+        } elseif ($errCode == 5 || $errCode == 6 || $errCode == 7){
+            $errors['img1'] = "An internal error occured while uploading Image 1";
+        }
+    } elseif(!in_array($_FILES['img1']['type'], $allowedTypes)){
+        //file type not allowed
+        $errors['img1'] = "Incorrect file type for Image 1.";
+    } elseif($_FILES['img1']['size'] > 1048576){ //file over 1MB
+        //file too big
+        $errors['img1'] = "Image 1 must be under 1MB";
+    } 
+    
+    $hasImg2 = false;
+    
+    if($_FILES['img2']['size'] > 0){
+        $hasImg2 = true;
+    
+        if($_FILES['img2']['error'] > 0){
+            //there is an error
+            $errCode = $_FILES['img2']['error'];
+            if($errCode == 1 || $errCode == 2){
+                $errors['img2'] = "The file uploaded for Image 2 is too large.";
+            } elseif($errCode == 3){
+                $errors['img2'] = "There was an error uploading Image 2";
+            } elseif ($errCode == 4){
+                $errors['img2'] = "No file was supplied for Image 2";
+            } elseif ($errCode == 5 || $errCode == 6 || $errCode == 7){
+                $errors['img2'] = "An internal error occured while uploading Image 2";
+            }
+        } elseif(!in_array($_FILES['img2']['type'], $allowedTypes)){
+            //file type not allowed
+            $errors['img2'] = "Incorrect file type for Image 2.";
+        } elseif($_FILES['img2']['size'] > 1048576){ //file over 1MB
+            //file too big
+            $errors['img2'] = "Image 2 must be under 1MB";
+        }
+    
+    }
+    
+    
     if(count($errors) > 0){
         output_form($errors, $petDetails);
     } else {
-        $insertSQL = "INSERT INTO cms_pets (CharityID, CreatorID, CreatedOn, Name, Description, Contact, Number1, Email, Location, isAdoptable)"
+        $insertSQL = "INSERT INTO cms_pets (CharityID, CreatorID, CreatedOn, EditorID, EditedOn, Name, Description, Contact, Number1, Email, Location, isAdoptable)"
                 . "VALUES((SELECT CharityID FROM cms_charities WHERE DomainName = '{$_SESSION['lastDomain']}'),"
-                . "{$_SESSION['userID']}, NOW(), '{$petDetails['Name']}', '{$petDetails['Description']}', '{$petDetails['ContactName']}', '{$petDetails['Number']}', '{$petDetails['Email']}', '{$petDetails['Location']}', 0);";
+                . "{$_SESSION['userID']}, NOW(), {$_SESSION['userID']}, NOW(), '{$petDetails['Name']}', '{$petDetails['Description']}', '{$petDetails['ContactName']}', '{$petDetails['Number']}', '{$petDetails['Email']}', '{$petDetails['Location']}', 0);";
         
         $insertResult = mysql_query($insertSQL);
         $petInsertID = mysql_insert_id();
@@ -62,7 +137,7 @@ if(isset($_POST['submission']) && $_POST['submission'] === "true"){ // the form 
         file_put_contents("C:\wamp\www\\3rdyearproj\images\pets\\" . $filename, $file);
         //move_uploaded_file seems to create the file, put doesn't moce the contents
         //move_uploaded_file($_FILES['img1']['tmp_name'], "C:\wamp\www\\3rdyearproj\images\lostAndFound\\" . $filename);
-        $imgSQL = "UPDATE cms_lostfounds SET Image1 = '{$filename}'";
+        $imgSQL = "UPDATE cms_pets SET Image1 = '{$filename}'";
         $petDetails['Image1'] = $filename;
         if($hasImg2){
             $path = $_FILES['img2']['name'];
@@ -77,11 +152,10 @@ if(isset($_POST['submission']) && $_POST['submission'] === "true"){ // the form 
         $petDetails['Image2'] = $filename;
         if($imgResult){
             //output success
-        
+    
         //INSERT INTO CMS_Content
         $insertContentSQL = "INSERT INTO CMS_Content VALUES (null, 'CMS_Pets', '" . $petInsertID . "')";
         $insertContentResult = mysql_query($insertContentSQL);
-        echo mysql_error();
         //GET ContentINsertID
         $insertContentID = mysql_insert_id();
         if($newStory || $existingStory){
@@ -99,7 +173,40 @@ if(isset($_POST['submission']) && $_POST['submission'] === "true"){ // the form 
             $insertStoryContentSQL = "INSERT INTO CMS_StoryContent VALUES (null, '". $insertStoryID . "', '" . $insertContentID . "')";
             $insertStoryContentResult = mysql_query($insertStoryContentSQL);
     }
-    
+           ?>
+<!-- Begin page content -->
+      <div class="container">
+        <div class="page-header">
+          <h1>Submit Resident</h1>
+        </div>
+          <div class="col-md-9">
+        <p class="lead">Your resident animal has been added to our database.</p>
+        <hr />
+        
+        <div class="well">
+            <img class="img-circle" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;" src="/images/pets/<?=$petDetails['Image1']?>" />
+              <h2>
+            <?=$petDetails['Name']?>
+            </h2>
+            <p>
+                Description:<br />
+                <?=$petDetails['Description']?>
+            </p>
+            Contact Name: <?=$petDetails['ContactName']?> <br />
+            Contact Number: <?=$petDetails['Number']?><br />
+            Contact Email: <?=$petDetails['Email']?>
+            <?php
+                if($hasImg2){
+                    echo '<img class="img-circle" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;" src="/images/pets/' . $petDetails['Image2'] . '" />';
+                }
+            ?>
+
+        </div>
+        
+          </div> <!-- /.col-md-9 -->
+            <?php
+        }
+    }
 } else {
     output_form($errors, $petDetails);
 }
