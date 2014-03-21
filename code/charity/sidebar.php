e1f10787d0379f9617c0370dd3135d0aa85a5fd9
cathaltoomey <111302591@umail.ucc.ie>
2014-03-21 00:08:53 +0000

    Update sidebar.php

--- code/charity/sidebar.php
+++ code/charity/sidebar.php
@@ -5,18 +5,6 @@
 <div class="col-md-3 sidebar">
               
               <div class="panel">
-                  Sidebar content goes in here! What the user wants, nobody knows! but we'll fill it up somehow!
-              </div>
-              
-              <div class="panel">
-                  The counter for total amount donated will look prettier when it's finished!
-              </div>
-              
-              <div class="panel">
-                  As a small charity we rely on donations from the public in order to achieve our goals.
-              </div>
-              
-              <div class="panel">
                   <?php// include '/charity/donateButton.html'; ?>
                   <?php require_once('/config.php'); ?>
  
